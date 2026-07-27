7f3db70e2627140765aad3ba7af5f78113c59857
cmone-git <one.cmeg@gmail.com>
2026-07-28 04:53:25 +0530

    Create sw.js

---
+++ taxapp/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'cm-taxapp-v5';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './assets/logo-name.png',
+  './assets/logo-splash.png',
+  './assets/logo-fav.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch new
+        return response || fetch(event.request);
+      })
+  );
+});
