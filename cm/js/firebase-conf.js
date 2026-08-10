882015dc858657728b12593b75ff30ac0963b26d
cmone-git <one.cmeg@gmail.com>
2026-08-10 23:25:49 +0530

    Create firebase-conf.js

---
+++ cm/js/firebase-conf.js
@@ -0,0 +1,10 @@
+// js/firebase-conf.js
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCXyKSTlmlzkYnH2LW408cVVWV1CPvlfBo",
+  authDomain: "cmfilings-6a37c.firebaseapp.com",
+  projectId: "cmfilings-6a37c",
+  storageBucket: "cmfilings-6a37c.firebasestorage.app",
+  messagingSenderId: "138705123778",
+  appId: "1:138705123778:web:9561bff9f0f5d89bb6fe5b"
+};
